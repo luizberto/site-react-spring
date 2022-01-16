7f0ea4ae2c0aad7ff86cb57d6ab5b762db2f78e4
luizberto <70718303+luizberto@users.noreply.github.com>
2022-01-16 16:03:51 -0300

    criação do frontend da aplicação

---
+++ siteApp/minhas-financas-app/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import 'bootswatch/dist/flatly/bootstrap.css' 
+import Login from './views/login';
+import './custom.css'
+
+class App extends React.Component {
+
+  render(){ 
+    return(
+     <Login/>
+    );
+  }
+}
+
+export default App;
